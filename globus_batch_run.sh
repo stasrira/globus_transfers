c24a9c205203266f4fbbbe57aa04e9339cd96e56
Stas Rirak <stasrira@yahoo.com>
2020-12-22 16:51:32 -0500

    Moves settings to a separate file. Adds functionality to use destination path prefixes in batch files. Adds a check to validate  if the Globus endpoint is running and starts it if it's not.

--- globus_batch_run.sh
+++ globus_batch_run.sh
@@ -1,37 +1,68 @@
-# This script will submit Globus transfer requests using a batch file specified in the "batchfile" parameter.
+# This script will submit Globus transfer requests using a batch file specified in the "batchfile" parameter of the setting file.
 # More details on this approach: https://docs.globus.org/cli/reference/transfer/
-# Batch file format example: <source path> <destination path> -r
+
+# Batch file format example (all values are separated with a single space): 
+# <source path> <destination path> -r
+#
 # "<source path>" - absolute path to the source directory to be transfered
 # "<destination path>" - path on the destination endpoint where to the directory have to be saved
 # "-r" - stands for recurcive; will copy all sub-folders recurcively and create needed structure on the destination
+
 #======================SETTINGS=====================================
-# verify the following settings before each transfer
-transfer_name=Transfer_20201221_set1 #custom name of the transfer to make it easier to find it in the Activity section of the Globus site
-batchfile=./globus_transfers/princenton_globus_batch_20201221_set1.txt #name of the batch file to be used for the current run
-
-# Existing endpoint ids
-# 24a98536-4202-11ea-9712-021304b0cca7 #Princeton lSI DTN endpoint
-# Specify destination endpoint id
-dest_ep=24a98536-4202-11ea-9712-021304b0cca7 #Princeton lSI DTN endpoint 
-
-#local directory and endpoint
-globus_wrk_dir=/ext_data/shared/.apps/globus_submissions #
-source_ep=a2cc0b26-46aa-11ea-b967-0e16720bb42f
-
+# load settings file
+. $(dirname "$0")/globus_batch.config
 
 #======================CODE=====================================
-#initialize virtual environment for Globus CLI installation (refer to: https://docs.globus.org/cli/installation/virtualenv/)
-# Stas account: "$HOME/.globus-cli-virtualenv/bin/activate"
-source "$HOME/.globus-cli-virtualenv/bin/activate" # this is a user specific installation directory of the Globus CLI
+# initialize virtual environment for Globus CLI installation (refer to: https://docs.globus.org/cli/installation/virtualenv/)
+# Stas' account: "$HOME/.globus-cli-virtualenv/bin/activate"
+#source "$HOME/.globus-cli-virtualenv/bin/activate" # this is a user specific installation directory of the Globus CLI
+echo "--> activate the virtual environment to run Globus"
+echo "$globus_virtual_dir/bin/activate"
+source "$globus_virtual_dir/bin/activate"
 
 cd $globus_wrk_dir
+echo "--> activate local endpoint"
 globus endpoint activate $source_ep #activate source (MSSM) endpoint
 
-#the following line is a dry-run for testing
-#globus transfer --dry-run --label $transfer_name --sync-level checksum -v $source_ep $dest_ep --batch < $batchfile
+#define name and create a temporary copy of the batch file
+batchfile_tmp=$batchfile".tmp"
+echo "--> create a temp copy of the batch file"
+echo cp $batchfile $batchfile_tmp
+cp $batchfile $batchfile_tmp
 
-#actual execution
-globus transfer --label $transfer_name --sync-level checksum -v $source_ep $dest_ep --batch < $batchfile
+# replace destination prefix in the batch file, if it can be found
+echo "--> search for a destination path prefix ($dest_pref_find_str) in the batch file an replace it with the following: $dest_pref_val"
+echo sed -i s+"$dest_pref_find_str"+"$dest_pref_val"+g $batchfile_tmp
+sed -i s+"$dest_pref_find_str"+"$dest_pref_val"+g $batchfile_tmp
 
-#deactivate virtual environment
+echo "--> submit Globus transfer request"
+# the following line is a dry-run for testing
+#globus transfer --dry-run --label $transfer_name --sync-level checksum -v $source_ep $dest_ep --batch < $batchfile_tmp
+
+# actual execution
+globus transfer --label $transfer_name --sync-level checksum -v $source_ep $dest_ep --batch < $batchfile_tmp
+
+#delete temp file
+echo "--> delete a temp copy of the batch file"
+echo rm $batchfile_tmp
+rm $batchfile_tmp
+
+echo "--> deactivate the virtual environment"
+# deactivate virtual environment
 deactivate
+
+#check if local Globus endpoint is running
+
+if ( /home/stas/globusconnectpersonal-2.3.8/globusconnectpersonal -status | grep -q "Globus Online:   connected" )
+then
+	echo "--> Local Globus endpoint is already running, no action is needed."
+else
+	echo "--> Local Globus endpoint is not running. Starting local Globus endpoint"
+	echo $start_local_endpoint
+	eval "$start_local_endpoint"
+fi
+
+#/home/stas/globusconnectpersonal-2.3.8/globusconnectpersonal -start -restrict-paths rw/ext_data/shared/ECHO,rw/data/stas,rw/home/stas &
+
+
+
